12394575f226a37737ae7b1ed925accccc1dca3e
TiranoMax <alexsandroarents@hotmail.com>
2018-09-26 20:00:18 -0300

    Configurado a routerconfig

--- Project_DisKWeb/Project_DisKWeb/App_Start/RouteConfig.cs
+++ Project_DisKWeb/Project_DisKWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Project_DisKWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Produto", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
