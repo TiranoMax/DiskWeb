fb6db79da4e64d683b90152b76caafc2f684b20e
TiranoMax <alexsandroarents@hotmail.com>
2018-09-26 19:54:03 -0300

    Criado o o DAO Singleton

---
+++ Project_DisKWeb/Project_DisKWeb/DAO/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using Project_DisKWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_DisKWeb.DAL
+{
+    public class Singleton
+    {
+        private static Context ctx;
+
+        private Singleton() { }
+
+        public static Context GetInstance()
+        {
+            if (ctx == null)
+            {
+                ctx = new Context();
+            }
+            return ctx;
+        }
+
+    }
+}
