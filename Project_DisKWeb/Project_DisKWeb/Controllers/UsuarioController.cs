03e0b3013a8dee90650defff81671262804f96b7
TiranoMax <alexsandroarents@hotmail.com>
2018-09-27 20:18:54 -0300

    Criado Api Interna com configuração no global asax

---
+++ Project_DisKWeb/Project_DisKWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Project_DisKWeb.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+    }
+}
