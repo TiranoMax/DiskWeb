d04763d17f456f6d2e5445f47ae0d2c125c1fcdc
TiranoMax <alexsandroarents@hotmail.com>
2018-09-27 20:21:24 -0300

    Configurado o modo padro de exibicao para json

--- Project_DisKWeb/Project_DisKWeb/App_Start/WebApiConfig.cs
+++ Project_DisKWeb/Project_DisKWeb/App_Start/WebApiConfig.cs
@@ -11,6 +11,9 @@ namespace Project_DisKWeb
         {
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.Indent = true;
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
